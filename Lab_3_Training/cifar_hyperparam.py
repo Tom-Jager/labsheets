434ab4ab207d49d9db593b4833795d2bc7b91f70
Tom-Jager <tjager22@gmail.com>
2018-10-22 21:36:04 +0100

    changed learning rate

--- Lab_3_Training/cifar_hyperparam.py
+++ Lab_3_Training/cifar_hyperparam.py
@@ -36,7 +36,7 @@ tf.app.flags.DEFINE_integer('save_model', 1000,
                             'Number of steps between model saves (default: %(default)d)')
 
 # Optimisation hyperparameters
-tf.app.flags.DEFINE_integer('batch_size', 16, 'Number of examples per mini-batch (default: %(default)d)')
+tf.app.flags.DEFINE_integer('batch_size', 256, 'Number of examples per mini-batch (default: %(default)d)')
 tf.app.flags.DEFINE_float('learning_rate', 1e-3, 'Learning rate (default: %(default)d)')
 tf.app.flags.DEFINE_integer('img_width', 32, 'Image width (default: %(default)d)')
 tf.app.flags.DEFINE_integer('img_height', 32, 'Image height (default: %(default)d)')
